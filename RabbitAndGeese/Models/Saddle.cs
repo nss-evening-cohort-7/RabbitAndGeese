45f36df87fc19d8a105426483d4518e5b55e481e
Nathan Gonzalez <ncgonz@gmail.com>
2018-10-06 12:43:32 -0500

    fake commit to cause a conflict

--- RabbitAndGeese/Models/Saddle.cs
+++ RabbitAndGeese/Models/Saddle.cs
@@ -8,8 +8,8 @@
         public SaddleType Type { get; set; }
         public bool InUse { get; set; }
         public bool Thing { get; set; }
-        public bool OtherThing { get; set; }
-        public bool MoreStuff { get; set; }
+        public bool Blerg { get; set; }
+        public bool Blarg { get; set; }
     }
 
     public enum SaddleType
